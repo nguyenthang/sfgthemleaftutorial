a1c42eafce8502cf1984ab2bf70ba60f62e49770
Thang <nguyenhongthangnl@gmail.com>
2016-05-26 12:55:05 +0700

    Initial commit

---
+++ src/main/java/sfg/tutorial/SfgThemleafTutorialApplication.java
@@ -0,0 +1,12 @@
+package sfg.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgThemleafTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgThemleafTutorialApplication.class, args);
+	}
+}
---
+++ src/test/java/sfg/tutorial/SfgThemleafTutorialApplicationTests.java
@@ -0,0 +1,18 @@
+package sfg.tutorial;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SfgThemleafTutorialApplication.class)
+@WebAppConfiguration
+public class SfgThemleafTutorialApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
